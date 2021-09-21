0cb6be1b7e7aa12e471b534ae0d3f2ed2fc5752b
Kai Fukushima <k_fukushima@yumemi.co.jp>
2021-09-21 09:46:01 +0900

    :sparkles: feat: create Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <Container className="Footer bg-dark py-5 text-white text-center" fluid>
+      <small>主催：ケルティック・シード・プロジェクト実行委員会</small>
+      <br />
+      <small>サイト作成：Kai Fukushima</small>
+    </Container>
+  );
+};
+
+export default Footer;
